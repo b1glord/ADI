2acc7e761c72ef48981645ab07d51ef1f08e22f4
Fury <FuryTr@yandex.com>
2018-10-19 12:23:44 +0300

    Update custom.sql

--- custom.sql
+++ custom.sql
@@ -1,20 +1,28 @@
-;start dota pvp ladder sql files 
+--- start dota pvp sql files 
 USE ragnarok;
-  create table pvpladder (
-		char_id int(11) not null default '0' primary key,
-		name varchar(30) not null default '',
-		streaks smallint(6) unsigned not null default '0',
-		kills smallint(6) unsigned not null default '0',
-		deaths smallint(6) unsigned not null default '0',
-		streaktime datetime
-	) engine = myisam;
-	
-	create table ownladder (
-		guild_id int(11) not null default '0' primary key,
-		name varchar(24) not null default '',
-		currentown smallint(6) unsigned not null default '0',
-		highestown smallint(6) unsigned not null default '0',
-		owntime datetime
-	) engine = myisam; 
-  
-;End dota pvp ladder sql files
+--
+-- Table structure for table `pvpladder`
+--
+
+CREATE TABLE IF NOT EXISTS `pvpladder` (
+  char_id int(11) not null default '0' primary key,
+  name varchar(30) not null default '',
+  streaks smallint(6) unsigned not null default '0',
+  kills smallint(6) unsigned not null default '0',
+  deaths smallint(6) unsigned not null default '0',
+  streaktime datetime
+) engine = MyISAM;
+
+--
+-- Table structure for table `ownladder`
+--
+
+CREATE TABLE IF NOT EXISTS `ownladder` (
+  guild_id int(11) not null default '0' primary key,
+  name varchar(24) not null default '',
+  currentown smallint(6) unsigned not null default '0',
+  highestown smallint(6) unsigned not null default '0',
+  owntime datetime
+) engine = MyISAM; 
+--- End Dota pvp Sql Files
+
